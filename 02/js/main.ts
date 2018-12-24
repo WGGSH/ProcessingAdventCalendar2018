7952f587c6c2ab999747f5414e49d90aec36a634
wgg <naokipgt@gmail.com>
2018-12-24 13:48:45 +0900

    完成

--- 02/js/main.ts
+++ 02/js/main.ts
@@ -1,13 +1,24 @@
 /// <reference path="p5.global-mode.d.ts" />
 
+let pDeviceOrientation: any;
+
 function setup(): void {
   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
 
   createCanvas(windowWidth, windowHeight);
+
+  pDeviceOrientation = deviceOrientation;
 }
 
 function draw(): void {
+  if (pDeviceOrientation !== undefined && pDeviceOrientation !== deviceOrientation) {
+    // 向きが変わったとき
+    noCanvas();
+    createCanvas(window.innerWidth, windowHeight);
+  }
+  pDeviceOrientation = deviceOrientation;
+
   background(0);
 
   if (mouseIsPressed) {
--- 03/js/main.ts
+++ 03/js/main.ts
@@ -1,35 +1,37 @@
 /// <reference path="p5.global-mode.d.ts" />
 
-let gyro: p5.Vector; // ジャイロセンサの値を格納する
+let pDeviceOrientation: any;
 
 function setup(): void {
-
-  gyro = new p5.Vector();
-  // ジャイロセンサの値を取得する
-  window.addEventListener("deviceorientation", function (event): void{
-    gyro.x = event.beta;
-    gyro.y = event.gamma;
-    gyro.z = event.alpha;
-  });
 
   createCanvas(windowWidth, windowHeight);
 
   background(0);
   colorMode(HSB, 360, 100, 100, 100);
+
+  pDeviceOrientation = deviceOrientation;
 }
 
 function draw(): void {
+
+  if (pDeviceOrientation !== undefined && pDeviceOrientation !== deviceOrientation) {
+    // 向きが変わったとき
+    noCanvas();
+    createCanvas(window.innerWidth, windowHeight);
+  }
+  pDeviceOrientation = deviceOrientation;
+
   blendMode(BLEND);
   background(0,5);
   blendMode(ADD);
 
   translate(windowWidth / 2, windowHeight / 2);
 
-  stroke(gyro.z, 85, 10);
+  stroke(rotationZ, 85, 10);
   strokeWeight(3);
-  // ジャイロセンサの値を元せ線の位置を指定する
+  // ジャイロセンサの値を元に線の位置を指定する
   let startPos: p5.Vector = new p5.Vector(0, 0);
-  let endPos: p5.Vector = new p5.Vector(gyro.y / 90 * width, -gyro.x / 90 * height);
+  let endPos: p5.Vector = new p5.Vector(rotationY / 90 * width, -rotationX / 90 * height);
   for (let i: number = 0; i < 25; i++){
     let startRandVec: p5.Vector = p5.Vector.random2D().mult(random(0, 10));
     let endRandVec: p5.Vector = p5.Vector.random2D().mult(random(0, 10));
